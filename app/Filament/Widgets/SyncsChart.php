54454e08b01decd7c79ffadabd93f61302b8fc40
Laura Brekelmans <mail@laura.fm>
2025-06-07 16:28:55 +0200

    Add sync chart

---
+++ app/Filament/Widgets/SyncsChart.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use App\Models\Sync;
+
+class SyncsChart extends ChartWidget
+{
+    protected static ?string $heading = 'Daily Syncs';
+
+    protected function getData(): array
+    {
+        $data = Sync::selectRaw('DATE(created_at) as day, COUNT(*) as syncs_count')
+            ->whereNotNull('created_at')
+            ->groupByRaw('DATE(created_at)')
+            ->orderByRaw('DATE(created_at) ASC')
+            ->get();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Syncs per day',
+                    'data' => $data->pluck('syncs_count')->toArray(),
+                    'borderColor' => '#3b82f6',
+                    'backgroundColor' => 'rgba(59, 130, 246, 0.1)',
+                ]
+            ],
+            'labels' => $data->pluck('day')->toArray(),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
