e1b129efa06bb069b5b37aba742c07914ac2f535
Laura Brekelmans <mail@laura.fm>
2025-07-01 15:23:50 +0200

    Remove Nunito font references from app.blade.php and app.scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ $title ?? "Scrybble Sync - Think analog, organize digital"}}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     @yield('head')
 
