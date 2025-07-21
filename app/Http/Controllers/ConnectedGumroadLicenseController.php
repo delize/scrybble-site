17fa1e0ec78905643dcf78664c10b9c01b64a8dc
Laura Brekelmans <mail@laura.fm>
2025-07-21 17:08:06 +0200

    Optimize imports

--- app/Http/Controllers/ConnectedGumroadLicenseController.php
+++ app/Http/Controllers/ConnectedGumroadLicenseController.php
@@ -3,19 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreConnectedGumroadLicenseRequest;
-use App\Models\GumroadLicense;
 use App\Models\User;
 use App\Services\GumroadApi;
 use App\Services\OnboardingStateService;
-use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use JsonException;
-use RuntimeException;
 
-class ConnectedGumroadLicenseController extends Controller {
-    public function store(StoreConnectedGumroadLicenseRequest $request, OnboardingStateService $onboarding_state_service, GumroadApi $gumroadApi) {
+class ConnectedGumroadLicenseController extends Controller
+{
+    public function store(StoreConnectedGumroadLicenseRequest $request, OnboardingStateService $onboarding_state_service, GumroadApi $gumroadApi)
+    {
         /** @var User $user */
         $user = Auth::user();
 
--- app/Http/Controllers/GumroadLicenseInformationController.php
+++ app/Http/Controllers/GumroadLicenseInformationController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\GumroadApi;
 use App\Services\GumroadService;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
-use Illuminate\Support\Facades\Auth;
 use JsonException;
 
 class GumroadLicenseInformationController extends Controller
